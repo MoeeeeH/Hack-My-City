bcf2b3d1356916348ecc7f556a3041dbd1827a8a
Florian Muehlhans <muehlhans.f@coflnet.com>
2025-05-08 19:55:45 +0200

    add a health endpoint

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 with open("events.json", "r", encoding="utf-8") as f:
     events = json.load(f)
 
+@app.get("/health")
+def health():
+    return "ok"
+
 @app.get("/api/events")
 def get_events():
     return events
