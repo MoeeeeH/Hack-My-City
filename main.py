8d92fb8caa56df195ab3c89d225bb662b871a19b
Florian Muehlhans <muehlhans.f@coflnet.com>
2025-05-08 21:07:45 +0200

    add cors middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import json
 from dotenv import load_dotenv
 from true import TrueRelevanceDataSource
@@ -6,6 +7,19 @@ from true import TrueRelevanceDataSource
 load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3001",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 trueRelevanceDataSource = TrueRelevanceDataSource()
 
